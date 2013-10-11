151154c82ded000d1979fb84c4f5e36959a0762c
Rodrigo Setti <rodrigosetti@gmail.com>
2013-10-11 00:06:52 -0700

    Lint fixes and comments

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,10 @@
 {-# LANGUAGE NoMonomorphismRestriction, FlexibleContexts #-}
 module Main where
 
-import Diagrams.Prelude
-import Diagrams.Backend.Cairo.CmdLine
 import Data.List (find)
 import Data.Maybe (fromJust)
+import Diagrams.Backend.Cairo.CmdLine
+import Diagrams.Prelude
 
 -- | Return True if the first number divides the second
 divides :: Integral a => a -> a -> Bool
@@ -14,22 +14,24 @@ divides a b = b `mod` a == 0
 primes :: [Integer]
 primes = sieve [2..] where
              sieve [] = []
-             sieve (x:xs) = x : (filter (not . divides x) $ sieve xs)
+             sieve (x:xs) = x : filter (not . divides x) (sieve xs)
 
 -- | Return the list of prime factors (with repetitions) for a positive integer
 factor :: Integer -> [Integer]
 factor n
     | n < 2 = []
     | otherwise = let d = fromJust $ find (`divides` n) primes in
-                   d : (factor $ n `div` d)
+                   d : factor (n `div` d)
 
+-- | Given an integer, returns it's factorization diagram
+factorDiagram :: (TrailLike (QDiagram b R2 m), Semigroup m) => Integer -> QDiagram b R2 m
 factorDiagram =
     compose . map fromInteger . factor
   where
-    compose [] = circle 2 # lw 0 # fc black
-    compose (x:xs) = foldl1 atop $ map ((`rotateBy` d') . Turn) [1/x, 2/x .. x/x] where
+    compose [] = circle 1 # lw 0 # fc black
+    compose (x:xs) = mconcat $ map ((`rotateBy` d') . Turn) [1/x, 2/x .. x/x] where
                       d' = if w > h then translateY r d else translateX r d
-                      r = (max w h) * 0.5 / sin (tau / (2 * x))
+                      r = max w h * 0.5 / sin (tau / (2 * x))
                       d = compose xs
                       w = width d
                       h = height d
